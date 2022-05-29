2a26b265d6d77e010b5015b3bde8bc95fc3554da
Jung, Jaewan <wodhkszxc@gmail.com>
2022-05-29 22:49:12 +0900

    views.py 작성

--- django_layout/introduceapp/views.py
+++ django_layout/introduceapp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+def department(request):
+    return render(request, 'department.html')
+
+def grid(request):
+    return render(request, 'grid.html')
+
+def html_css(request):
+    return render(request, 'html_css.html')
+
+def main(request):
+    return render(request, 'main.html')
+
+def timetable(request):
+    return render(request, 'timetable.html')

4364b5c41227ddac8e6ad35620311126765e01c0
Jung, Jaewan <wodhkszxc@gmail.com>
2022-07-05 01:10:29 +0900

    ...

--- mydiary2/myapp/views.py
+++ mydiary2/myapp/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import DiaryForm
+from django.utils import timezone
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def create(request):
+    if request.method == 'POST':
+        form = DiaryForm(request.POST)
+        if form.is_valid():
+            form = form.save(commit=False)
+            form.pub_date = timezone.now()
+            form.save()
+            return redirect('index')
+        
+        else:
+            form = DiaryForm
+            return render(request, 'create.html', {'form':form})
--- mydiary3/myproject/urls.py
+++ mydiary3/myproject/urls.py
@@ -15,10 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-import myapp.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', myapp.views.index, name='index'),
-    path('create/', myapp.views.create, name='create'),
 ]

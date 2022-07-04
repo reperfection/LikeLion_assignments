e1600e185f0bb3cb57591152c1fd5d4d2cfdf3d3
reperfection <wodhkszxc@gmail.com>
2022-07-04 17:52:30 +0900

    일단 커밋

--- mydiary/myproject/settings.py
+++ mydiary/myproject/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!]
-secret_sfile = os.path.join(BASE_DIR, 'secrets.json')
+secret_file = os.path.join(BASE_DIR, 'secrets.json')
 
 with open(secret_file) as f:
     secrets = json.loads(f.read())

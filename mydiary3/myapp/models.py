dffc6cb56e8b9765e6a0f3022b8335ba772bbf19
Jung, Jaewan <wodhkszxc@gmail.com>
2022-07-05 15:47:19 +0900

    처음부터 다시 커밋

---
+++ mydiary3/myapp/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Diary(models.Model):
+    title = models.CharField(max_length = 100)
+    pub_date = models.DateTimeField('data published')
+    content = models.TextField()
+    
+    def __str__(self):
+        return self.title
---
+++ mydiary3/myapp/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import DiaryForm
+from django.utils import timezone
+from .models import Diary
+
+# Create your views here.
+def main(request):
+    return render(request, 'main.html')
+
+def create(request):
+    if request.method == 'POST':
+        form = DiaryForm(request.POST)
+        if form.is_valid():
+            form = form.save(commit=False)
+            form.pub_date = timezone.now()
+            form.save()
+            return redirect('main')
+        
+    else:
+        form = DiaryForm
+        return render(request, 'create.html', {'form':form})
+    
+def read(request):
+    diarys = Diary.objects
+    return render(request, 'read.html', {'diarys': diarys})
+
+def detail(request, id):
+    diary = get_object_or_404(Diary, id=id)
+    return render(request, 'detail.html', {'diary':diary})

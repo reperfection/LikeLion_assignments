404694c4f47d79b81e5ed0a52b637bb4f4da00aa
Jung, Jaewan <wodhkszxc@gmail.com>
2022-07-05 15:24:31 +0900

    views.py 수정

--- mydiary3/myapp/views.py
+++ mydiary3/myapp/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import DiaryForm
+from django.utils import timezone
+from .models import Diary
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def create(request):
+    if request.method == 'POST':
+        form = DiaryForm(request.POST)
+        if form.is_valid():
+            form = form.save(commit=False)
+            form.pub_date = timezone.now()
+            form.save()
+            return redirect('index')
+        
+    else:
+        form = DiaryForm
+        return render(request, 'create.html', {'form':form})
+    
+def read(request):
+    diarys = Diary.objects
+    return render(request, 'read.html', {'diarys': diarys})
+
+def detail(request, id):
+    diary = get_object_or_404(Diary, id=id)
+    return render(request, 'detail.html', {'diary':diary})

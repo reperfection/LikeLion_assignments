17951e50861c068a9216cfd965b9785697956aa2
Jung, Jaewan <wodhkszxc@gmail.com>
2022-07-11 11:31:10 +0900

    edit, delete, static 추가

--- mydiary3/myproject/urls.py
+++ mydiary3/myproject/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 import myapp.views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,4 +25,6 @@ urlpatterns = [
     path('write/', myapp.views.write, name='write'),
     path('read/', myapp.views.read, name='read'),
     path('detail/<str:id>/', myapp.views.detail, name='detail'),
-]
+    path('edit/<str:id>/', myapp.views.edit, name='edit'),
+    path('delete/<str:id>/', myapp.views.delete, name='delete'),
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)

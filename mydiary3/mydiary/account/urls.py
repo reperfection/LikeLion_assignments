d96bd2ada934a8f7ecb10601571edb18a9a0fd68
Jung, Jaewan <wodhkszxc@gmail.com>
2022-08-14 17:42:54 +0900

    error

--- mydiary3/mydiary/account/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from account import views
-
-urlpatterns = [
-    path('signup/', views.signup, name='signup'),
-    path('login/', views.login, name='login'),
-    path('logout/', views.logout, name='logout'),
-]
